43ad610aa867667e10ae6f24b3d014f4c2fa2d79
Dan Haugen <dbhaug11@smumn.edu>
2013-02-03 23:33:31 -0600

    alphamon started

--- CS325/src/assignments/frs/hotgammon/alphamon/GameImpl.java
+++ CS325/src/assignments/frs/hotgammon/alphamon/GameImpl.java
@@ -1,7 +1,7 @@
 package assignments.frs.hotgammon.alphamon;
 
 /** Skeleton implementation of HotGammon.
- 
+
    This source code is from the book 
      "Flexible, Reliable Software:
        Using Patterns and Agile Development"
@@ -10,22 +10,56 @@ package assignments.frs.hotgammon.alphamon;
      Henrik B Christensen 
      Computer Science Department
      Aarhus University
-   
+
    This source code is provided WITHOUT ANY WARRANTY either 
    expressed or implied. You may study, use, modify, and 
    distribute it for non-commercial purposes. For any 
    commercial use, see http://www.baerbak.com/
-*/
-
+ */
 public class GameImpl implements Game {
-  public void newGame() {}
-  public void nextTurn() {}
-  public boolean move(Location from, Location to) { return false; }
-  public Color getPlayerInTurn() { return Color.NONE; }
-  public int getNumberOfMovesLeft() { return 0; }
-  public int[] diceThrown() { return new int[] {1,1}; }
-  public int[] diceValuesLeft() { return new int []{}; }
-  public Color winner() { return Color.NONE; }
-  public Color getColor(Location location) { return Color.NONE; }
-  public int getCount(Location location) { return 0; }
+	private Color playerInTurn=Color.NONE;
+	private int[][] board;
+	private int numberOfMoves;
+
+	public void newGame() {
+		board=new int[2][28];
+		for(int i=0;i<board.length;i++){
+			board[1][i]=0;
+			board[0][i]=0;
+		}
+		board[1][0]=1;
+		board[1][25]=-1;
+		board[1][26]=1;
+		board[1][27]=-1;
+		board[0][1]=2;
+		board[1][1]=1;
+		board[0][24]=2;
+		board[1][24]=-1;
+	}
+	public void nextTurn() {
+		playerInTurn=Color.BLACK;
+		numberOfMoves = 2;
+	}
+	public boolean move(Location from, Location to) {
+		if(board[1][to.ordinal()]==-playerInTurn.getSign()){
+			return false;
+		}
+		board[0][from.ordinal()]--;
+		board[0][to.ordinal()]++;
+		numberOfMoves--;
+		return true;
+	}
+	public Color getPlayerInTurn() { return playerInTurn; }
+	public int getNumberOfMovesLeft() {
+		return numberOfMoves;
+	}
+	public int[] diceThrown() { return new int[] {1,1}; }
+	public int[] diceValuesLeft() { return new int []{}; }
+	public Color winner() { return Color.NONE; }
+	public Color getColor(Location location) {
+		return Color.getColorFromNumerical(board[1][location.ordinal()]);
+	}
+	public int getCount(Location location) { 
+		return board[0][location.ordinal()];
+	}
 }
---
+++ CS325/src/assignments/frs/hotgammon/alphamon/TestAlphamon.java
@@ -0,0 +1,57 @@
+package assignments.frs.hotgammon.alphamon;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/** Testing skeleton for AlphaMon.
+
+   This source code is from the book 
+     "Flexible, Reliable Software:
+       Using Patterns and Agile Development"
+     published 2010 by CRC Press.
+   Author: 
+     Henrik B Christensen 
+     Computer Science Department
+     Aarhus University
+
+   This source code is provided WITHOUT ANY WARRANTY either 
+   expressed or implied. You may study, use, modify, and 
+   distribute it for non-commercial purposes. For any 
+   commercial use, see http://www.baerbak.com/
+ */
+public class TestAlphamon {
+	private Game game;
+
+	@Before public void setup() {
+		game = new GameImpl();
+	}
+
+	@Test 
+	public void shouldHaveNoPlayerInTurnAfterNewGame() {
+		game.newGame();
+		assertEquals( Color.NONE, game.getPlayerInTurn() );
+	}
+	@Test 
+	public void shouldHaveBlackPlayerInTurnAfterNewGame() {
+		game.newGame();
+		game.nextTurn(); // will throw [1,2] and thus black starts
+		assertEquals( Color.BLACK, game.getPlayerInTurn() );
+	}
+	@Test
+	public void shouldHaveTwoBlackCheckersOnR1(){
+		game.newGame();
+		game.nextTurn();
+		assertTrue(game.getCount(Location.R1)==2);
+		assertTrue(game.getColor(Location.R1)==Color.BLACK);
+	}
+	@Test
+	public void shouldBeAbleToMoveBlackR1toR2(){
+		game.newGame();
+		game.nextTurn();
+		assertTrue(game.move(Location.R1, Location.R2));
+		assertTrue(game.getCount(Location.R1)==1);
+		assertTrue(game.getCount(Location.R2)==1);
+		assertTrue(game.getNumberOfMovesLeft()==1);
+		assertFalse(game.move(Location.R1, Location.B1));
+	}
+}
